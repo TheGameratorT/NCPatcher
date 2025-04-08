1db7c63f80c26cd314ff565bc62604dc04bd55d1
tiagolsilva <2221436@my.ipleiria.pt>
2025-04-08 13:09:39 +0100

    Update to latest rapidjson*
    Fixes compilation on newer GCC versions

--- vendor/rapidjson.cmake
+++ vendor/rapidjson.cmake
@@ -5,7 +5,7 @@ ExternalProject_Add(
     rapidjson
     PREFIX "vendor/rapidjson"
     GIT_REPOSITORY "https://github.com/Tencent/rapidjson.git"
-    GIT_TAG f54b0e47a08782a6131cc3d60f94d038fa6e0a51
+    GIT_TAG 24b5e7a8b27f42fa16b96fc70aade9106cf7102f
     TIMEOUT 10
     CMAKE_ARGS
         -DRAPIDJSON_BUILD_TESTS=OFF
